82fd543a5adbe791bb17d7980684aad66b209eaa
Bereketmelese1 <bereket21bnm@gmail.com>
2024-05-11 21:48:28 +0300

    Add new feature

---
+++ src/views/AboutView.vue
@@ -0,0 +1,206 @@
+<template>
+  <div>
+    <header class="border-b-2 mt-8 mb-3">
+      <label>
+        <span class="ml-6 text-blue-700 text-3xl">G</span>
+        <span class="text-red-500 text-3xl">o</span>
+        <span class="text-yellow-400 text-3xl">o</span>
+        <span class="text-blue-700 text-3xl">g</span>
+        <span class="text-green-700 text-3xl">l</span>
+        <span class="text-red-500 text-3xl">e</span>
+      </label>
+
+      <from class="relative">
+        <i class="absolute inset-y-0 right-5 flex items-center pr-3 fas fa-search text-gray-400"></i>
+        <input class="m-4 border rounded-3xl w-96 py-2 px-3 text-gray-700" type="text" value="bahirdar"
+          placeholder="search Google">
+      </from>
+
+      <div class="flex m-3 float-right">
+        <i class="mr-6 fas fa-cog text-gray-600 text-xl"></i>
+        <i class="mr-6 fas fa-th text-gray-600 text-xl"></i>
+        <button class="bg-blue-500 w-24">Sign in</button>
+      </div>
+
+
+
+      <div class="mx-32">
+        <ul class="flex  mb-6">
+          <i class="flex flex-row items-center fas fa-search text-gray-600"></i>
+          <li class="ml-1 mr-4">All</li>
+          <i class="flex flex-row items-center fas fa-image text-gray-600"></i>
+          <li class="ml-1 mr-4">Images</li>
+          <i class="flex flex-row items-center fas fa-video text-gray-600"></i>
+          <li class="ml-1 mr-4">Videos</li>
+          <i class="flex flex-row items-center fas fa-newspaper text-gray-600"></i>
+          <li class="ml-1 mr-4">News</li>
+          <i class="flex flex-row items-center fas fa-map-marker-alt text-gray-600"></i>
+          <li class="ml-1 mr-4">Maps</li>
+          <i class="flex flex-row items-center fas fa-ellipsis-v text-gray-600"></i>
+          <li class="ml-1 mr-4">More</li>
+        </ul>
+      </div>
+    </header>
+
+    <div class="mx-32">
+
+      <div class="container scroll-mx-10 flex">
+        <div class="w-2/3 px-4">
+          <p><span class="text-gray-400">About 3,900,000 results (0.44 seconds) </span>
+          <p> https:/en.wikipedia.org<span class="text-gray-400">> wiki > Bahir_Dar</span> </p>
+          </p>
+          <p class="block text-blue-600 text-2xl">
+            Bahir Dar - Wikipedia
+          </p>
+          <p>
+            Bahir Dar is the capital city of Amhara Region, Ethiopia. Bahir Dar is one of the leading tourist
+            destinations in Ethiopia, with a variety of attractions in ..
+          </p>
+          <div class="container my-4">
+
+            <span class="text-gray-400">zone: </span> Bahirdar special zone
+            <span class="flex float-right mr-32"><span class="text-gray-400">Elevation:</span> 1,800m (5,900 ft)</span>
+            <span class="text-gray-400"><br>
+              country:</span> Ethiopia
+            <span class="flex float-right mr-32"><span class="text-gray-400">Area codes:</span> (+251) 58</span>
+
+          </div>
+
+          <div class="text-blue-600">
+            History - Geography - Demography - Education
+          </div>
+
+          <div>
+            <p class="mt-10 text-4xl">People also ask
+              <i class="ml-3 fas fa-ellipsis-v text-gray-600"></i>
+            </p>
+            <table class="m-5 border-collapse">
+              <tr>
+                <td class="w-full p-2 border border-l-0 border-r-0">What is the English of Bahir Dar?
+                  <i class="float-right fas fa-angle-down text-gray-600"></i>
+                </td>
+              </tr>
+              <tr>
+                <td class="p-2 border border-l-0 border-r-0">How many people live in Bahir Dar, Ethiopia?
+                  <i class="float-right fas fa-angle-down text-gray-600"></i>
+                </td>
+              </tr>
+              <tr>
+                <td class="p-2 border border-l-0 border-r-0">What is the sister city of Bahir Dar?
+                  <i class="float-right fas fa-angle-down text-gray-600"></i>
+                </td>
+              </tr>
+              <tr>
+                <td class="p-2 border border-l-0 border-r-0">How many Kebeles are there in Bahir Dar?
+                  <i class="ml-96 float-right fas fa-angle-down text-gray-600"></i>
+                </td>
+              </tr>
+            </table>
+          </div>
+          <p class="mt-10 text-4xl">
+            <i class="fas fa-image text-gray-600"></i>
+            Images for bahirdar
+            <i class="ml-3 fas fa-ellipsis-v text-gray-600"></i>
+          </p>
+          <div class="flex mt-5">
+
+            <button class="flex items-center justify-center rounded-3xl border border-gray-400 mr-2 w-31 h-12">
+              <img src="../assets/bd-1.jfif" alt="Button Image" class="rounded-2xl w-8 h-8 mr-2">
+              beautifull
+            </button>
+            <button class="flex items-center justify-center rounded-3xl border border-gray-400 mr-2 w-24 h-12">
+              <img src="../assets/bd-1.jfif" alt="Button Image" class="rounded-2xl w-8 h-8 mr-2">
+              city
+            </button>
+
+            <button class="flex items-center justify-center rounded-3xl border border-gray-400 mr-2 w-24 h-12">
+              <img src="../assets/hot.jpg" alt="Button Image" class="rounded-2xl w-8 h-8 mr-2">
+              kuriftu
+            </button>
+
+            <button class="flex items-center justify-center rounded-3xl border border-gray-400 mr-2 w-24 h-12">
+              <img src="../assets/hot.jpg" alt="Button Image" class="rounded-2xl w-8 h-8 mr-2">
+              hotel
+            </button>
+            <button class="flex items-center justify-center w-36 h-12 rounded-3xl border border-gray-400 mr-2">
+              <img src="../assets/nile.jfif" alt="Button Image" class="mr-2 rounded-2xl w-8 h-8">
+              blue nile falls
+            </button>
+            <button class="flex items-center justify-center rounded-3xl border border-gray-400 mr-2 w-24 h-12">
+              <img src="../assets/lake.jfif" alt="Button Image" class="rounded-2xl w-8 h-8 mr-2">
+              lake
+            </button>
+            <button class="flex items-center justify-center rounded-3xl border border-gray-400 mr-2 w-12 h-12">
+              <i class="float-right fas fa-angle-down text-gray-600"></i>
+            </button>
+
+          </div>
+
+          <div class="flex mt-3 rounded-2xl">
+            <img class="w-44" src="../assets/bd-1.jfif" alt="">
+            <img class="w-44" src="../assets/hot.jpg" alt="">
+            <img class="w-44" src="../assets/nile.jfif" alt="">
+            <img class="w-44" src="../assets/lake.jfif" alt="">
+          </div>
+          <div class="m-6 "><span
+              class="mr-1 text-gray-500 tracking-[-3px]">-----------------------------------------------------------------</span>
+            <button class="border rounded-xl items-center w-64">View all</button>
+            <span
+              class="text-gray-500 mr-1 tracking-[-3px]">--------------------------------------------------------------------</span>
+          </div>
+        </div>
+
+        <div class="w-1/3 px-4 border rounded-lg mt-10 mr-10">
+
+          <div class="mt-10 flex">
+            <img class="w-48" src="../assets/bd-2.jpg" alt="bahirdar images">
+            <img class="w-48" src="../assets/bd-1.jfif" alt="bahirdar images">
+          </div>
+
+          <div class="text-3xl">
+            Bahir Dar <br>
+            <span class="text-gray-400 text-xs">City in ethiopia</span>
+          </div>
+
+          <div>
+            <p>
+              About
+
+              Bahir Dar is the capital city of the Amhara
+              region in northern Ethiopia.It's a port on the
+              south shore of the huge inland Lake Tana.
+              Dek Island is one of many islands in the lake
+              that are home to medieval monasteries.
+              On the Zege Peninsula, the Ura Kidane
+              Mihret monastery is known for its
+              elaborate, colorful murals. ― Google
+
+              <br>
+            <div class="py-9">
+              <p class="my-3">
+                Area: 213.4 km²<br>
+              </p>
+              <p class="my-3">
+                University: Bahir Dar University peda campus <br>
+              </p>
+              <p class="my-3">
+                Elevation: 1,800 m <br>
+              </p>
+              <p class="my-3">
+                Area code: (+251) 58 <br>
+              </p>
+              <p class="my-3">
+                Region: Amhara
+              </p>
+            </div>
+            </p>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,70 @@
+<script>
+export default {
+  data() {
+    return {
+      searchQuery: "",
+    };
+  },
+  methods: {
+    handleSearchInput() {
+      if (this.searchQuery.toLowerCase() === "bahirdar") {
+        this.$router.push({ name: "about" });
+      } else {
+        this.$router.push({ name: "k" });
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <div
+      class="flex flex-row items-center float-right text-sm text-gray-700 mt-5"
+    >
+      <p href="" class="p-3">Gmail</p>
+      <a href="" class="p-3">Images</a>
+      <button class="bg-blue-600 text-white w-24 h-8 mr-8">Log in</button>
+    </div>
+
+    <div class="flex flex-col justify-center items-center h-screen">
+      <div class="flex flex-col justify-center items-center h-screen">
+        <label>
+          <span class="text-blue-500 text-8xl">G</span>
+          <span class="text-red-500 text-8xl">o</span>
+          <span class="text-yellow-500 text-8xl">o</span>
+          <span class="text-blue-500 text-8xl">g</span>
+          <span class="text-green-500 text-8xl">l</span>
+          <span class="text-red-500 text-8xl">e</span>
+        </label>
+
+        <div class="mb-5 mt-7 items-center relative">
+          <i
+            class="absolute inset-y-0 left-5 flex items-center pl-3 fas fa-search text-gray-400"
+          ></i>
+          <input
+            v-model="searchQuery"
+            @keyup.enter="handleSearchInput"
+            class="m-4 shadow-lg border rounded-3xl w-[650px] py-2 px-3 text-gray-700 pl-10"
+            type="text"
+            placeholder="search Google"
+          />
+          <div class="absolute inset-y-0 right-5 flex items-center pr-3">
+            <i class="mr-3 fas fa-keyboard text-gray-400"></i>
+            <i class="fas fa-microphone text-gray-400"></i>
+          </div>
+        </div>
+
+        <div class="mb-10 flex items-center justify-center">
+          <button class="bg-gray-100 py-2 px-4 ml-4">Google Search</button>
+          <button class="bg-gray-100 py-2 px-4 ml-4">I'm Feeling Lucky</button>
+        </div>
+        <div class="inline">
+          Google offered in :<span class="text-blue-600">
+            Amharic Tigirigna Somaalli</span
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
